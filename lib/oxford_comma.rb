d094ff4f04824e37b96d73e5084d74f830c9f8da
Justin Luciano <justinlluciano@gmail.com>
2022-01-31 12:39:32 -0800

    commit tree 48b4f3df3c35c0778e238fe2f0deb2ba05949af0

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    str = ''
+    array[0...-1].each { |word| str += "#{word}, " }
+    str + "and #{array[-1]}"
+  end
 end
